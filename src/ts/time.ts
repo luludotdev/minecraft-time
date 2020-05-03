b38300ddc97092a54ff249211c7b2cb46c0c51dd
Jack Baron <jackmbaron@gmail.com>
2020-05-04 00:14:11 +0100

    use the minecraft epoch

--- src/ts/time.ts
+++ src/ts/time.ts
@@ -1,7 +1,11 @@
 export const time = document.getElementById('time')
 if (time === null) throw new Error('Time element is null!')
 
-export const getTime = () => (Date.now() / 50).toFixed(0)
+// Earliest recorded Minecraft version
+// May 10, 2009
+const MINECRAFT_EPOCH = 1241910000000
+
+export const getTime = () => ((Date.now() - MINECRAFT_EPOCH) / 50).toFixed(0)
 
 const setTime = () => {
   time.innerHTML = getTime()
