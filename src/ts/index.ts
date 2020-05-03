f65b247d14bab2fdb1be6c10528af5d5a95f7e68
Jack Baron <jackmbaron@gmail.com>
2020-05-03 02:08:28 +0100

    copy time to clipboard on click

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,14 +1,20 @@
+import copy from 'copy-to-clipboard'
+
 import '../styles/fonts.styl'
 import '../styles/styles.styl'
 
 const time = document.getElementById('time')
 if (time === null) throw new Error('Time element is null!')
 
-const setTime = () => {
-  const ticks = (Date.now() / 50).toFixed(0)
-  time.innerHTML = ticks
+const getTime = () => (Date.now() / 50).toFixed(0)
 
+const setTime = () => {
+  time.innerHTML = getTime()
   requestAnimationFrame(setTime)
 }
 
 requestAnimationFrame(setTime)
+
+time?.addEventListener('click', () => {
+  copy(getTime())
+})
