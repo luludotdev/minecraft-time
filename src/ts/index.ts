62854a4b3ad776c62b6320f86df4934fe36cee6a
Jack Baron <jackmbaron@gmail.com>
2020-05-04 00:22:08 +0100

    extract tooltip styles to own file

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,6 +2,7 @@ import copy from 'copy-to-clipboard'
 
 import '../styles/fonts.styl'
 import '../styles/styles.styl'
+import '../styles/tooltip.styl'
 
 import { getTime, time } from './time'
 import './tooltip'
