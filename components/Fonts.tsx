68c2eae4afb69c017272abe674742afed89ef46d
Jack Baron <jackmbaron@gmail.com>
2022-03-26 10:56:49 +0000

    add fonts

---
+++ components/Fonts.tsx
@@ -0,0 +1,25 @@
+import { type FC } from 'react'
+
+export const Fonts: FC<{ children?: never }> = () => (
+  <style jsx global>{`
+    @font-face {
+      font-family: 'Minecraft';
+      src: url('/fonts/Minecraft-Regular.woff2') format('woff2'),
+        url('/fonts/Minecraft-Regular.woff') format('woff');
+
+      font-weight: normal;
+      font-style: normal;
+    }
+
+    html {
+      font-family: 'Minecraft';
+      font-size: 40px;
+    }
+
+    @media screen and (max-width: 600px) {
+      html {
+        font-size: 20px;
+      }
+    }
+  `}</style>
+)
